ded7ded4779a5f818e97a78c9bf7a3b0d64a3696
fsackur <freddie.sackur@hotmail.com>
2017-04-10 16:33:09 +0100

    Minor refactor

--- Tests/SslRegConfig.Pester.Tests.ps1
+++ Tests/SslRegConfig.Pester.Tests.ps1
@@ -1,19 +1,20 @@
-﻿Import-Module $PSScriptRoot\..\Private\SSlRegConfig.psm1 -Force
+﻿$ModuleName = 'SSlRegConfig'
+Import-Module $PSScriptRoot\..\Private\$ModuleName -Force -ErrorAction Stop
 
 $Global:RegParentPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL'
-$Global:NotFoundException = New-Object System.Management.Automation.ItemNotFoundException ("Cannot find path '$RegParentPath\Protocols\TLS 1.1\Server' because it does not exist.")
+$Global:Tls11NotFoundException = New-Object System.Management.Automation.ItemNotFoundException ("Cannot find path '$RegParentPath\Protocols\TLS 1.1\Server' because it does not exist.")
 $Global:MOCK_TLS_11_REGKEY_EXISTS = $false
 
 
 Describe 'Reg functions' {
-    Mock 'Get-ItemProperty' -ModuleName SslRegConfig -MockWith {
+    Mock 'Get-ItemProperty' -ModuleName $ModuleName -MockWith {
         switch ($LiteralPath) {
             "$RegParentPath\Protocols\SSL 3.0\Server"
                 {return New-Object psobject -Property @{Enabled=0}}
             "$RegParentPath\Protocols\TLS 1.0\Server"
                 {return New-Object psobject -Property @{Enabled=1}}
             "$RegParentPath\Protocols\TLS 1.1\Server"
-                {throw $NotFoundException}
+                {throw $Tls11NotFoundException}
         }
     }
     
@@ -24,17 +25,17 @@ Describe 'Reg functions' {
     $Tls11Enable  = {$LiteralPath -eq "$RegParentPath\Protocols\TLS 1.1\Server" -and $Value -eq 1}
     $Tls11Disable = {$LiteralPath -eq "$RegParentPath\Protocols\TLS 1.1\Server" -and $Value -eq 0}
 
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -MockWith {}
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -ParameterFilter $Ssl30Enable -MockWith {}
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -ParameterFilter $Tls10Enable -MockWith {}
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -ParameterFilter $Tls10Disable -MockWith {}
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -ParameterFilter $Tls11Disable -MockWith {if (-not $MOCK_TLS_11_REGKEY_EXISTS) {throw $NotFoundException}}
-    Mock 'Set-ItemProperty' -ModuleName SslRegConfig -ParameterFilter $Tls11Enable -MockWith {if (-not $MOCK_TLS_11_REGKEY_EXISTS) {throw $NotFoundException}}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -MockWith {}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -ParameterFilter $Ssl30Enable -MockWith {}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -ParameterFilter $Tls10Enable -MockWith {}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -ParameterFilter $Tls10Disable -MockWith {}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -ParameterFilter $Tls11Disable -MockWith {if (-not $MOCK_TLS_11_REGKEY_EXISTS) {throw $Tls11NotFoundException}}
+    Mock 'Set-ItemProperty' -ModuleName $ModuleName -ParameterFilter $Tls11Enable -MockWith {if (-not $MOCK_TLS_11_REGKEY_EXISTS) {throw $Tls11NotFoundException}}
 
     Context 'Getting reg values' {
         It 'Returns reg value if present, or null if not present' {
             $RegValues = Get-SslRegValues
-            Assert-MockCalled 'Get-ItemProperty' -ModuleName SslRegConfig
+            Assert-MockCalled 'Get-ItemProperty' -ModuleName $ModuleName
             $RegValues['TLS1.0'] | Should BeExactly 1
             $RegValues['SSL3.0'] | Should BeExactly 0
             $RegValues['TLS1.1'] | Should Be $null
@@ -45,7 +46,7 @@ Describe 'Reg functions' {
     Context 'Getting reg report' {
         It 'Returns reg value if present, or null if not present' {
             $RegValues = Get-SslRegValues
-            Assert-MockCalled 'Get-ItemProperty' -ModuleName SslRegConfig
+            Assert-MockCalled 'Get-ItemProperty' -ModuleName $ModuleName
             $RegValues['TLS1.0'] | Should BeExactly 1
             $RegValues['SSL3.0'] | Should BeExactly 0
             $RegValues['TLS1.1'] | Should Be $null
@@ -69,26 +70,26 @@ Describe 'Reg functions' {
         
         $Global:MockWmiProvider = New-Object psobject -Property @{Name = 'StdRegProv'}
         $MockWmiProvider | Add-Member ScriptMethod -Name 'CreateKey' -Value $MockWmiCreateRegKey
-        Mock 'Get-WmiObject' -ModuleName SslRegConfig -ParameterFilter {$List -and $Namespace -eq "ROOT\DEFAULT"} -MockWith {return $MockWmiProvider}
+        Mock 'Get-WmiObject' -ModuleName $ModuleName -ParameterFilter {$List -and $Namespace -eq "ROOT\DEFAULT"} -MockWith {return $MockWmiProvider}
 
         It 'Sets reg values if provided' {
             $MOCK_TLS_11_REGKEY_EXISTS | Should Be $false
             Set-SslRegValues -Enable 'SSL3.0' -Disable 'TLS1.1'
-            Assert-MockCalled 'Set-ItemProperty' -ModuleName SslRegConfig -Times 1 -ParameterFilter $Ssl30Enable
-            Assert-MockCalled 'Set-ItemProperty' -ModuleName SslRegConfig -Times 1 -ParameterFilter $Tls11Disable
+            Assert-MockCalled 'Set-ItemProperty' -ModuleName $ModuleName -Times 1 -ParameterFilter $Ssl30Enable
+            Assert-MockCalled 'Set-ItemProperty' -ModuleName $ModuleName -Times 1 -ParameterFilter $Tls11Disable
             $MOCK_TLS_11_REGKEY_EXISTS | Should Be $true
         }
         
         It 'Gives precedence to Enable values over Disable' {
             Set-SslRegValues -Enable 'TLS1.0' -Disable 'TLS1.0'
-            Assert-MockCalled 'Set-ItemProperty' -ModuleName SslRegConfig -Times 0 -ParameterFilter $Tls10Disable
-            Assert-MockCalled 'Set-ItemProperty' -ModuleName SslRegConfig -Times 1 -ParameterFilter $Tls10Enable
+            Assert-MockCalled 'Set-ItemProperty' -ModuleName $ModuleName -Times 0 -ParameterFilter $Tls10Disable
+            Assert-MockCalled 'Set-ItemProperty' -ModuleName $ModuleName -Times 1 -ParameterFilter $Tls10Enable
         }
         
         It 'Creates new reg key if needed' {
 
             Set-SslRegValues -Enable 'TLS1.1'
-            Assert-MockCalled 'Get-WmiObject' -ModuleName SslRegConfig -Times 1
+            Assert-MockCalled 'Get-WmiObject' -ModuleName $ModuleName -Times 1
         }
     }
     
@@ -118,7 +119,7 @@ Describe 'Reg backup' {
 Describe 'Function help' {
     Context 'Correctly-formatted help' {
         It 'Provides examples for every exported function' {
-            foreach ($Command in (Get-Module SslRegConfig | select -ExpandProperty ExportedCommands).Keys) {
+            foreach ($Command in (Get-Module $ModuleName | select -ExpandProperty ExportedCommands).Keys) {
                 $Command | Should BeOfType string
                 (Get-Help $Command -Examples | Out-String) | Should Match '-------------------------- EXAMPLE 1 --------------------------'
 
@@ -130,7 +131,7 @@ Describe 'Function help' {
 Describe 'Parameter validation' {
     Context 'Enable and Disable parameters' {
 
-        Mock 'Set-ItemProperty' -ModuleName SslRegConfig {}
+        Mock 'Set-ItemProperty' -ModuleName $ModuleName {}
 
         It 'Rejects garbage' {
             #{New-SslRegValues -Enable 'asdgasgasgasfasf'}  | Should Throw "Cannot validate argument on parameter"
@@ -139,7 +140,7 @@ Describe 'Parameter validation' {
             {Set-SslRegValues -Disable 'asdgasgasgasfasf'} | Should Throw "Cannot validate argument on parameter"
         }
 
-        InModuleScope 'SslRegConfig' {
+        InModuleScope $ModuleName {
             It 'Accepts any key from lookup table of supported elements' {
                 $RegLookup = Get-SslRegLookupTable
                 foreach ($Key in $RegLookup.Keys) {
