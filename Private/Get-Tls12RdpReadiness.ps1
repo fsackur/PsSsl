c24f7b7541cf26c4739a09fa389dcd6f1b3cf73d
Freddie Sackur <github@dustyfox.uk>
2018-08-22 18:13:07 +0100

    Created skeleton helper functions

---
+++ Private/Get-Tls12RdpReadiness.ps1
@@ -0,0 +1,40 @@
+﻿function Get-Tls12RdpReadiness
+{
+    <#
+        .SYNOPSIS
+        Audits readiness for disabling TLS 1.0 and below.
+
+        .DESCRIPTION
+        Audits readiness for disabling TLS 1.0 and below.
+
+        If updates are required, they will be reported in the output.
+
+        .OUTPUTS
+        [psobject]
+
+        .EXAMPLE
+        Get-Tls12RdpReadiness
+
+    #>
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    param ()
+
+    begin
+    {
+        Write-Verbose "[$(Get-Date)] Begin :: $($MyInvocation.MyCommand)"
+    }
+
+    process
+    {
+        $Output = New-Object PSObject -Property @{
+            SupportsTls12   = $false
+            RequiredUpdates = @()
+        }
+    }
+
+    end
+    {
+        Write-Verbose "[$(Get-Date)] End   :: $($MyInvocation.MyCommand)"
+    }
+}
