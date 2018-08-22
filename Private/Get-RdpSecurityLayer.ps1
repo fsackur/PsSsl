aae2e997ab4c8901e56d539ee28a44ea955abed7
Freddie Sackur <github@dustyfox.uk>
2018-08-22 18:38:57 +0100

    New function Get-RdpSecurityLayer

---
+++ Private/Get-RdpSecurityLayer.ps1
@@ -0,0 +1,30 @@
+function Get-RdpSecurityLayer
+{
+    <#
+    .SYNOPSIS
+    Gets the RDP security layer.
+
+    .DESCRIPTION
+    Gets the RDP security layer.
+
+    .OUTPUTS
+    [string]
+
+    Returns 'Rdp', 'Negotiate', or 'Tls'.
+
+    .LINK
+    https://docs.microsoft.com/en-us/windows/desktop/termserv/win32-tsgeneralsetting
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param ()
+
+    $WmiObject = Get-WmiObject -Namespace "ROOT\CIMV2\TerminalServices" -Query "SELECT SecurityLayer FROM Win32_TSGeneralSetting"
+    switch ($WmiObject.SecurityLayer)
+    {
+        0       {return 'Rdp'}
+        1       {return 'Negotiate'}
+        2       {return 'Tls'}
+        default {throw "Error determining RDP security layer."}
+    }
+}
