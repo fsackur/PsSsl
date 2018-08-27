19b18af7e265f4a45b34ee04baddbea905cb15e9
Freddie Sackur <github@dustyfox.uk>
2018-08-27 16:52:16 +0100

    New function New-TlsProtocolObject

---
+++ Private/New-TlsProtocolObject.ps1
@@ -0,0 +1,97 @@
+function New-TlsProtocolObject
+{
+    <#
+        .SYNOPSIS
+        Creates an object representing a version of a TLS protocol.
+
+        .DESCRIPTION
+        Creates an object representing a version of a TLS protocol.
+
+        .PARAMETER Protocol
+        Specify the SSL / TLS protocol.
+
+        .PARAMETER OSVersion
+        To avoid a duplicate WMI call, provide the semantic version of the operating system.
+
+        .OUTPUTS
+        [psobject]
+
+        .EXAMPLE
+        New-TlsProtocolObject 'TLS 1.2 Client'
+
+        Protocol       IsEnabled OsDefaultIsEnabled
+        --------       --------- ------------------
+        TLS 1.2 Client      True               True
+
+        Gets the TLS 1.2 Client protocol.
+    #>
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateSet(
+            'SSL 2.0 Client',
+            'SSL 2.0 Server',
+            'SSL 3.0 Client',
+            'SSL 3.0 Server',
+            'TLS 1.0 Client',
+            'TLS 1.0 Server',
+            'TLS 1.1 Client',
+            'TLS 1.1 Server',
+            'TLS 1.2 Client',
+            'TLS 1.2 Server',
+            'DTLS 1.0 Client',
+            'DTLS 1.0 Server',
+            'DTLS 1.2 Client',
+            'DTLS 1.2 Server'
+        )]
+        [string]$Protocol,
+
+        [Parameter()]
+        [version]$OSVersion = ([version](Get-WmiObject -Query "SELECT Version FROM Win32_OperatingSystem").Version)
+    )
+
+    begin
+    {
+        $OutputProperties = 'Protocol', 'IsEnabled', 'OsDefaultIsEnabled'
+
+        $DefaultsForOS    = Get-TlsProtocolDefaults -OSVersion $OSVersion
+        $SchannelKey      = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL"
+        $ProtocolsKey     = "$SchannelKey\Protocols"
+    }
+
+    process
+    {
+        $Output = New-Object psobject
+        foreach ($Property in $OutputProperties)
+        {
+            Add-Member -InputObject $Output -MemberType 'NoteProperty' -Name $Property -Value $null
+        }
+
+        $Output.Protocol           = $Protocol
+
+        $DefaultForOs              = $DefaultsForOS.$Protocol
+        $OsDefaultIsEnabled        = $DefaultForOS -eq 'Enabled'
+        $Output.OsDefaultIsEnabled = $OsDefaultIsEnabled
+
+
+        $SubKey    = $Protocol -replace ' (?=(Client|Server)$)', '\'
+        $RegKeyObj = Registry\Get-RegKey "$ProtocolsKey\$SubKey"
+
+        if ($null -eq $RegKeyObj)
+        {
+            $Output.IsEnabled = $OsDefaultIsEnabled
+        }
+        else
+        {
+            $Output.IsEnabled = (
+                $RegKeyObj.DisabledByDefault -eq 0 -and
+                ($RegKeyObj.Enabled -band 1) -eq 1      # Check the right-most bit, instead of simple numeric equality, due to community confusion around 0xffffffff versus 1
+            )
+        }
+
+
+        return $Output
+    }
+}
