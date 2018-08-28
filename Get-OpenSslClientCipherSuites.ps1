159240c3cb95b0e1fc596c797c16e86c967ae96c
mich8638.cust@intensive.int <mich8638.cust@intensive.int>
2018-08-28 12:26:58 +0000

    New function Get-OpenSslClientCipherSuites

---
+++ Get-OpenSslClientCipherSuites.ps1
@@ -0,0 +1,10 @@
+﻿function Get-OpenSslClientCipherSuites
+{
+    [CmdletBinding()]
+    param ()
+
+    $OpenSslText = Invoke-OpenSsl -ArgumentList "ciphers", 'ALL:eNULL'
+    $CipherSuites = $OpenSslText -split ':'
+
+    return $CipherSuites
+}
